f563793eb3ecd4e6bb39e0b9467921fd486f8765
Dag_Hjermann <dhj@niva.no>
2019-10-10 23:47:03 +0200

    Init script 34
    Downloading data using functions

---
+++ 34_Profile_data_Python_functions.py
@@ -0,0 +1,107 @@
+def read_profile_python(input_lon, input_lat, variables):
+  import numpy as np     # Package for scientific computing 
+  import pandas as pd    # Package for data frames 
+  import matplotlib.pyplot as plt   
+  from datetime import datetime,timedelta
+  from netCDF4 import Dataset #  This is handy for working with netCDF files
+  import roppy
+
+  # Connect to server
+  filepath = 'http://thredds.met.no/thredds/dodsC/metusers/arildb/MARTINI800_prov_v2.ncml' # The OPENDAP URL
+  filehandle = Dataset(filepath) # open for reading 
+  grid = roppy.SGrid(filehandle) # Create a grid object for our file - used to get depth layers
+
+  # Create handle for variables
+  lon = filehandle.variables['lon_rho']
+  lat = filehandle.variables['lat_rho']
+  ocean_time = filehandle.variables['ocean_time']
+
+  # We need to find the grid location closest to the selected coordinates.
+  # For the sake of simplicity, let's calculate the sum of the absolute 
+  # differences between all grid points latitude and longitude and the 
+  # selected coordinates. 
+  position_diff = np.abs( lat[:] - input_lat ) + np.abs( lon[:] - input_lon )
+
+  # This line will find the indices of the minimum value in 
+  i, j = np.unravel_index( position_diff.argmin(), position_diff.shape )
+
+  print('Grid indices of grid point closest to selected location: {}, {}\n'.format(i, j))
+  print('Grid point longitude: {}'.format(lon[i,j]))
+  print('Grid point latitude: {}'.format(lat[i,j]))
+
+  # Depth layer (uses the grid object created by roppy.SGrid)
+  Z = grid.z_w[: ,i,j]
+  
+  # max of the time dimension
+  max_time = len(filehandle.dimensions["ocean_time"])
+
+  # Pick times (for now, we choose all of them)
+  i_time = np.arange(0,max_time,1) 
+
+  # temp = potential temperaturen
+  # salt = salinity
+  # N1_p = phosphate/phosphorus
+  # N3_n = nitrate/nitrogen
+  # chla = chlorophyll A
+
+  loc_time = filehandle.variables['ocean_time'][i_time]
+  
+  # loc_variable = filehandle.variables[variable_name][i_time,:,i,j]
+  
+  value_list = []
+  for var in variables:
+    values = [filehandle.variables[var][i_time,:,i,j]]
+    value_list.append(values)
+
+  # Turn times into datetimes
+  # expected_format = 'seconds since %Y-%m-%d %H:%M:%S'
+  # timeref = datetime.strptime(ocean_time.units, expected_format) # Creating a datetime object for the reference time
+  # datetime_list = np.array([ timeref + timedelta(seconds=t) for t in loc_time  ])
+  # loc_datetime = np.array(datetime_list)
+
+  # result = {'time_unix':loc_time, 'values':loc_variable, 'i':i, 'j':j, 'lon':lon[i,j], 'lat':lat[i,j], 'Z':Z}
+  result = {'time_unix':loc_time, 'variables':variables, 'values':value_list, 'i':i, 'j':j, 'lon':lon[i,j], 'lat':lat[i,j], 'Z':Z}
+
+  return result
+
+def get_unit(variable):
+  try:
+    unit = filehandle.variables[variable].units
+  except:
+    unit = "NA"
+  return unit
+
+  
+def variable_info():
+  import numpy as np
+  import pandas as pd
+  from netCDF4 import Dataset #  This is handy for working with netCDF files
+
+  # Connect to server
+  filepath = 'http://thredds.met.no/thredds/dodsC/metusers/arildb/MARTINI800_prov_v2.ncml' # The OPENDAP URL
+  filehandle = Dataset(filepath) # open for reading 
+  keys = filehandle.variables.keys()
+  print('Number of variables:', len(keys))
+  print()
+
+  names = [(filehandle.variables[key].name) for key in keys]
+  #names = np.array(names)
+
+  long_names = [(filehandle.variables[key].long_name) for key in keys]
+  #long_names = np.array(long_names)
+  
+  units = [get_unit(key) for key in keys]
+  
+  # result = pd.DataFrame({'keys':keys, 
+  #                        'long_names':long_names})
+  result = pd.DataFrame({'name':np.array(names), 'long_name':np.array(long_names), 'unit':np.array(units)})
+  
+  #
+  # UNCOMMENT TO SEE THE WHOLE LIST 
+  #
+  # for key in keys:
+  #   print(key, '=', filehandle.variables[key].long_name)
+  
+  return result
+
+
---
+++ 34_Profile_data_R_functions.R
@@ -0,0 +1,45 @@
+#
+# Read data using Python function
+# Returns list (including a time variable)
+#
+read_profile_list <- function(input_lon, input_lat, variable_name){
+  result <- read_profile_python(input_lon, input_lat, variable_name)
+  # Remove one level from value list
+  for (i in seq_along(result$values))
+    result$values[[i]] <- result$values[[i]][[1]]
+  # Set names of value list
+  names(result$values) <- result$variables
+  # Make time variable ready to use in plots
+  result$time <- as.POSIXct(result$time_unix, origin = "1970-01-01", tz = "UTC")
+  result
+}
+
+
+#
+# Takes list returned by 'read_profile_list()' and returns a data frame
+#   in 'long' format (suitable for ggplot)
+#
+profile_list2dataframe <- function(list){
+  depth_lo <- head(list$Z,-1)              # all values except the last
+  depth_hi <- tail(list$Z,-1)              # all values except the first
+  depth_mid <- (depth_lo + depth_hi)/2     # mid depth
+  
+  n_var <- length(list$values)
+  df <- matrix(NA, nrow = length(list$values[[1]]), ncol = n_var)
+  for (i in 1:n_var)
+    df[,i] <- as.numeric(list$values[[i]])
+  df <- as.data.frame(df)
+  colnames(df) <- list$variables
+  
+  # Make data frame
+  data.frame(
+    Time = list$time,                                       # time has length 364, but will just repeat itself
+    Depth_lo = rep(depth_lo, each = length(list$time)),     # value no. 1 is repeteated 364 times, etc.
+    Depth_hi = rep(depth_hi, each = length(list$time)),     #    "
+    Depth_mid = rep(depth_mid, each = length(list$time)),   #    "
+    df
+  )
+  
+}
+
+
