c5f55b964e6b35bd0ec157873ba3a1769548ea56
Dag_Hjermann <dhj@niva.no>
2019-10-11 15:01:20 +0200

    Add function read_profiles, script 34 R func
    Uses repeated calls to read_profile(). Perhaps confusing to use so similar names (read_profile and read_profiles)

--- 34_Profile_data_R_functions.R
+++ 34_Profile_data_R_functions.R
@@ -2,10 +2,14 @@
 # Read profile data from OpenDAP server
 # Returns a data frame in 'long' format (suitable for ggplot)
 #
+# This function uses the functions 'read_profile_list' (which in turn uses the Python function 'read_profile_python')
+#   and 'profile_list2dataframe'
+# This function is used by the function 'read_profiles' (which calls 'read_profile' repeatedly)
+#
 read_profile <- function(input_lon, input_lat, 
                          variable_names,
                          server_url = 'http://thredds.met.no/thredds/dodsC/metusers/arildb/MARTINI800_prov_v2.ncml'){
-  X <- read_profile_list(10.5268, 59.0267, c('temp','salt'), server_url)
+  X <- read_profile_list(input_lon, input_lat, variable_names, server_url)
   profile_list2dataframe(X)
 }
 
@@ -53,6 +57,66 @@ profile_list2dataframe <- function(list){
     Depth_mid = rep(depth_mid, each = length(list$time)),   #    "
     df
   )
+  
+}
+
+#
+# Read profile data for several stations  
+# 
+# Compulsory input:
+#   input_data: data frame with at least 3 variables: one 'ID' column (e.g., station name), longitude and latitude
+#   id_column: the name of the variable to be used for ID (e.g. id_column = "Station")
+#   variable_names: text vector of variable names tha twe want to extract
+# Optional input:
+#   lon_column: if not given, the function tries to search for a variable name including 'lon'; if not found, it returns an error message
+#   lat_column: if not given, the function tries to search for a variable name including 'lat'; if not found, it returns an error message
+#   server_url: URL to the thredds server
+# Returns:
+#   A data frame similar to the one returned by 'read_profile()', but with an extra colymn named 'ID'
+#
+read_profiles <- function(input_data,  
+                          id_column,
+                          variable_names,
+                          lon_column = NULL,
+                          lat_column = NULL,
+                          server_url = 'http://thredds.met.no/thredds/dodsC/metusers/arildb/MARTINI800_prov_v2.ncml'){
+  
+    k <- grep("lon", colnames(input_data), ignore.case = TRUE)[1]
+    if (length(k) == 0)
+      stop("You need to specify the name of the variable with longitude using lon_column")
+    lon_column <- colnames(input_data)[k]
+    
+    k <- grep("lat", colnames(input_data), ignore.case = TRUE)[1]
+    if (length(k) == 0)
+      stop("You need to specify the name of the variable with latitude using lon_column")
+    lat_column <- colnames(input_data)[k]
+    
+    result <- 1:nrow(input_data) %>% map_df(~profile_one_station(., input_data, id_column, lon_column, lat_column))
+    
+    result
+}
+
+  
+
+# Function for getting data from a single station - for use in 'read_profiles()'   
+# - the main reason that we make a special function for this is to add a variable for Station
+# The function's input is the row number 'i' of 'df_stations'
+# The function returns a data frame with the data (using read_profile) and an extra variable for Station  
+profile_one_station <- function(i, input_data, id_column, lon_column, lat_column){
+  
+    data <- 
+    # Get data 
+    read_profile(input_lon = df_stations[i, lon_column], 
+                 input_lat = df_stations[i, lat_column], 
+                 c('N1_p','N3_n')
+    ) %>%
+    # Add 'ID' variable 
+    mutate(ID = df_stations[i, id_column]) %>%
+    # Puts ID variable as variable number 1 (not really needed)
+    select(ID, everything())
+  
+  # The function returns this as result
+  data
   
 }
 
